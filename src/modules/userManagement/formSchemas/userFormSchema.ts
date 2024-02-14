79ea0a251711a0fdbbe4a747febdb14070ac7ea8
sianida26 <chesakeselekkebo@gmail.com>
2024-02-14 17:04:21 +0700

    lint

--- src/modules/userManagement/formSchemas/userFormSchema.ts
+++ src/modules/userManagement/formSchemas/userFormSchema.ts
@@ -6,18 +6,21 @@ export interface UserFormData {
 	photoProfileUrl: string;
 	email: string;
 	password: string | undefined;
-	roles: string[]
+	roles: string[];
 }
 
-const userFormDataSchema = z.object({
-	id: z.string().optional(),
-	name: z.string(),
-	photoProfileUrl: z.union([z.string().url(), z.null(), z.string()]),
-	email: z.string().email(),
-	password: z.string().optional(),
-	roles: z.array(z.string())
-  }).refine((data) => data.id || data.password || data.password!.length >= 8, {
-	message: "Password is required and must be at least 8 characters long if id is empty",
-	path: ["password"],
-  });
+const userFormDataSchema = z
+	.object({
+		id: z.string().optional(),
+		name: z.string(),
+		photoProfileUrl: z.union([z.string().url(), z.null(), z.string()]),
+		email: z.string().email(),
+		password: z.string().optional(),
+		roles: z.array(z.string()),
+	})
+	.refine((data) => data.id || data.password || data.password!.length >= 8, {
+		message:
+			"Password is required and must be at least 8 characters long if id is empty",
+		path: ["password"],
+	});
 export default userFormDataSchema;
