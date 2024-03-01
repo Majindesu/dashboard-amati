419ebf3d5157c8871d800715b84947db7832a258
sianida26 <chesakeselekkebo@gmail.com>
2024-03-01 21:02:59 +0700

    Added content type

---
+++ src/types/ContentType.d.ts
@@ -0,0 +1,14 @@
+type ContentType =
+	| "application/json"
+	| "application/x-www-form-urlencoded"
+	| "multipart/form-data"
+	| "text/plain"
+	| "text/html"
+	| "application/xml"
+	| "text/xml"
+	| "application/pdf"
+	| "image/jpeg"
+	| "image/png"
+	| "image/gif";
+
+export default ContentType;
