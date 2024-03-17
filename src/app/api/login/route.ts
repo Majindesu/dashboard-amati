ad8f465a4fd315c6a2993a2e3688039b8391434a
sianida26 <chesakeselekkebo@gmail.com>
2024-03-17 10:33:40 +0700

    Added cookie set on login

--- src/app/api/login/route.ts
+++ src/app/api/login/route.ts
@@ -3,29 +3,30 @@ import handleCatchApi from "@/core/utils/handleCatchApi";
 import AuthError from "@/modules/auth/error/AuthError";
 import signInSchema from "@/modules/auth/formSchemas/signInSchema";
 import signIn from "@/modules/auth/services/signIn";
+import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
 export async function POST(request: NextRequest) {
 	try {
-		applicationJsonOnly(request.headers)
+		applicationJsonOnly(request.headers);
 		const data = signInSchema.safeParse(await request.json());
 
-        if (!data.success){
-            throw new AuthError({
-                errorCode: "INVALID_CREDENTIALS",
-                message: "Email or Password does not match",
-                statusCode: 401
-            })
-        }
+		if (!data.success) {
+			throw new AuthError({
+				errorCode: "INVALID_CREDENTIALS",
+				message: "Email or Password does not match",
+				statusCode: 401,
+			});
+		}
 
-		const result = await signIn(data.data)
+		const result = await signIn(data.data);
 
-		request.cookies.set("token", result.token)
+		cookies().set("token", result.token);
 
 		return NextResponse.json(result);
 	} catch (e) {
-		return handleCatchApi(e)
+		return handleCatchApi(e);
 	}
 }
